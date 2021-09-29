e74d64e056a3c568d297b648991318973d9adf5d
nedwardsthro <edwanc18@wfu.edu>
2021-09-28 23:33:14 -0400

    9/28 update

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Thesis_Work
 
 9/21/21 - Downloaded Basketball Reference Data and set up github
+
+603 rows of traded players, 39 double traded, 1 triple traded
+603+39+1
+643 +4762
+
+5512 in the original Sloan Article. there are 5405 in ours. In data that matters (players with more than 30 games), we have slightly 68 more observations, likely again due to the traded feature, but working the opposite direction now. Say a player plays 25 games with one team and 25 games with another. The Sloan article won't pick that player up because he didn't play more than 30 games for either team. We will pick that player up because it will show up as 50 games for us. 
+
+Because of the data structure (our data having one line per season even if the player is traded vs. their data likely having multiple lines per season if the player is traded), our density plots will be different. It can be shown from their density plot that 
--- Trial Script.R
+++ Trial Script.R
@@ -1,5 +1,63 @@
 # Honors Thesis Work
 library(tidyverse)
 library(nbastatR)
-stats_2018<-bref_players_stats(seasons = 2018, tables = c("totals", "per_game", "advanced","per_minute", "per_poss"),join_data = TRUE)
-bref_stats<-bref_players_stats(seasons = 2008:2018, tables = c("totals", "per_game", "advanced", "per_minute", "per_poss"))
+Sys.setenv("VROOM_CONNECTION_SIZE" = 131072*2)
+bref_stats<-bref_players_stats(seasons = 2009:2018, tables = c("totals", "per_game", "advanced", "per_minute", "per_poss"))
+
+# Evaluate if we have the correct number of players)
+not_traded<-bref_stats%>%filter(slugTeamBREF != "TOT")
+traded<-bref_stats%>%filter(slugTeamBREF == "TOT")
+traded_twice <- traded%>%filter(nchar(slugTeamsBREF) >9)
+traded_thrice<-traded_twice%>%filter(nchar(slugTeamsBREF) > 15)
+
+Count_Games<-bref_stats%>%filter(countGames > 30)
+try1<-game_data%>%filter(gp>30)
+
+# Load NBA data through NBA.com
+trial<-teams_players_stats(seasons = 2009:2018, types = "player", tables = c("general"), measures = c('Base', 'Advanced', "Misc", 'Scoring', 'Usage'), mode = "Per100Possessions")
+tria2<-teams_players_stats(seasons = 2009:2018, types = "player", tables = c("shots"), measures = c('Base', 'Advanced', "Misc", 'Scoring', 'Usage'), mode = "Per100Possessions")
+
+
+Trial1<-trial[1:10,]
+Trial2<-trial[11:20,]
+Trial3<-trial[21:30,]
+Trial4<-trial[31:40,]
+Trial5<-trial[41:50,]
+
+load_data<-function(dataset){
+  game_data<-data.frame()
+  for(n in (1:nrow(dataset))){
+    year = dataset[[4]][[n]]
+    df = dataset[[7]][[n]]
+    df$Season = year
+    game_data <- rbind(game_data, df)
+    print(n)
+  }
+  return(game_data)
+}
+
+data1<-load_data(Trial1)
+data2<-load_data(Trial2)
+data3<-load_data(Trial3)
+data4<-load_data(Trial4)
+data5<-load_data(Trial5)
+
+master<-inner_join(data1, data2, by = c('idPlayer', 'idTeam', 'Season'), suffix = c('_Base', '_Adv'))%>%
+  inner_join(data3, by = c('idPlayer', 'idTeam', 'Season'), suffix = c('', '_Misc'))%>%
+  inner_join(data4, by = c('idPlayer', 'idTeam', 'Season'), suffix = c('', '_Scoring'))%>%
+  inner_join(data5, by = c('idPlayer', 'idTeam', 'Season'), suffix = c('', '_Usage'))
+
+master%>%filter(gp_Base >30)
+
+### Show an example of a player who was traded midseason
+player <- bref_stats%>%filter(namePlayer == "Evan Turner")
+
+### Density Chart
+plot1<-ggplot(master, aes(x = gp_Base))+
+  geom_density()
+plot2<-ggplot(traded, aes(x = countGames))+
+  geom_density()
+plot3<-ggplot(not_traded, aes(x = countGames))+
+  geom_density()
+plot1
+
