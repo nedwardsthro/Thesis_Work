3d5a8b20569f41c1506c91561a86c35e06b8402d
nedwardsthro <edwanc18@wfu.edu>
2021-10-06 00:23:07 -0400

    closes #3 closes #4

--- README.md
+++ README.md
@@ -8,4 +8,16 @@
 
 5512 in the original Sloan Article. there are 5405 in ours. In data that matters (players with more than 30 games), we have slightly 68 more observations, likely again due to the traded feature, but working the opposite direction now. Say a player plays 25 games with one team and 25 games with another. The Sloan article won't pick that player up because he didn't play more than 30 games for either team. We will pick that player up because it will show up as 50 games for us. 
 
-Because of the data structure (our data having one line per season even if the player is traded vs. their data likely having multiple lines per season if the player is traded), our density plots will be different. It can be shown from their density plot that 
+Because of the data structure (our data having one line per season even if the player is traded vs. their data likely having multiple lines per season if the player is traded), our density plots will be different.
+
+Update 10/6 below
+
+See new Geom_density with scraped data
+
+Used Mclust
+Also useful mclustBIC and mclustBICupdate
+mclustModel
+
+Potentially looking into doing dimension reduction in the future?
+
+
--- Trial Script.R
+++ Trial Script.R
@@ -4,6 +4,7 @@ library(nbastatR)
 Sys.setenv("VROOM_CONNECTION_SIZE" = 131072*2)
 bref_stats<-bref_players_stats(seasons = 2009:2018, tables = c("totals", "per_game", "advanced", "per_minute", "per_poss"))
 
+Shooting_Scrape <- read_csv("~/Desktop/Fall 2021/Honors Thesis Work/Basketball Reference Scrape.csv")
 # Evaluate if we have the correct number of players)
 not_traded<-bref_stats%>%filter(slugTeamBREF != "TOT")
 traded<-bref_stats%>%filter(slugTeamBREF == "TOT")
@@ -17,6 +18,7 @@ try1<-game_data%>%filter(gp>30)
 trial<-teams_players_stats(seasons = 2009:2018, types = "player", tables = c("general"), measures = c('Base', 'Advanced', "Misc", 'Scoring', 'Usage'), mode = "Per100Possessions")
 tria2<-teams_players_stats(seasons = 2009:2018, types = "player", tables = c("shots"), measures = c('Base', 'Advanced', "Misc", 'Scoring', 'Usage'), mode = "Per100Possessions")
 
+bref_stats<-bref_players_stats(seasons = 2009:2018, tables = c("per_poss"))
 
 Trial1<-trial[1:10,]
 Trial2<-trial[11:20,]
@@ -53,9 +55,9 @@ master%>%filter(gp_Base >30)
 player <- bref_stats%>%filter(namePlayer == "Evan Turner")
 
 ### Density Chart
-plot1<-ggplot(master, aes(x = gp_Base))+
+plot1<-ggplot(Shooting_Scrape, aes(x = G))+
   geom_density()
-plot2<-ggplot(traded, aes(x = countGames))+
+plot2<-ggplot(bref_stats, aes(x = countGames))+
   geom_density()
 plot3<-ggplot(not_traded, aes(x = countGames))+
   geom_density()
